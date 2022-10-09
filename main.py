42509ccb1be5b9e733d8bdaba3969302f415feb7
Nicholas <87298104+NicholasTc@users.noreply.github.com>
2022-10-08 20:17:49 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,15 @@
 # ******************************
 strval = input().split()
 numbers = []
-for v in strval:
-	numbers.append(int(v))
-print (numbers)
+
+for n in strval:
+    numbers.append(int(n))
+
+num = int(input())
+reoccur = numbers.count(num)
+
+print(reoccur)
+
 # the below llin 11 are same as the lines from 5 to 8
 # numbers = list(map(int, strval))
 # print (numbers)
